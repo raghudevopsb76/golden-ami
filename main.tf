9ba2e6c058e17752e944057d921be27d338b71f0
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-28 07:07:04 +0530

    TF Code

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "null_resource" "install" {
 
 resource "aws_ami_from_instance" "ami" {
   depends_on = [null_resource.install]
-  name               = "golden-ami"
+  name               = "golden-ami-v${formatdate("MMDDYYYY",timestamp())}"
   source_instance_id = aws_instance.ami.id
 }
 
