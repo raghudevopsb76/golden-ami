691755f201c718c7ad03ef9fb900fe647c9208d5
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-28 06:49:51 +0530

    TF Code

--- main.tf
+++ main.tf
@@ -7,3 +7,16 @@ resource "aws_instance" "ami" {
   }
 }
 
+resource "null_resource" "install" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = var.root_password
+      host     = aws_instance.ami.private_ip
+    }
+    inline = [
+      "labauto ansible"
+    ]
+  }
+}
